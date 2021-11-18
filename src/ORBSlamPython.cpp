703ef7a60a04fef8b4edf92ac244f4f1af4f7b86
Fraser McGhan <fwm1998@gmail.com>
2021-11-18 13:47:05 +0000

    fix typo

--- src/ORBSlamPython.cpp
+++ src/ORBSlamPython.cpp
@@ -382,7 +382,7 @@ bool ORBSlamPython::getKeyframeNeeded() const
         return false;
     }
 
-    return system->GetKeyframeNeeded();
+    return system->getKeyframeNeeded();
 }
 
 boost::python::list ORBSlamPython::getKeyframePoints() const
